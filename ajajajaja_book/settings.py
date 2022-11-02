1028988a427aff3bb19eba8236bf3689966cea64
turtas8 <turatbek.imankulov@gmail.com>
2022-11-02 17:19:25 +0600

    cors

--- ajajajaja_book/settings.py
+++ ajajajaja_book/settings.py
@@ -151,6 +151,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
     "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
 ]
 
 # Default primary key field type
