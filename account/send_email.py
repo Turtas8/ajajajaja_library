92cd6eeafb0f1a2358293b40395b3935b65ea40e
turtas8 <turatbek.imankulov@gmail.com>
2022-11-03 10:39:47 +0600

    123123123

--- account/send_email.py
+++ account/send_email.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 
 
 def send_confirmation_email(user, code):
-    full_link = f'http://localhost:8000/api/v1/accounts/activate/{code}/'
+    full_link = f'http://34.73.108.209/api/v1/accounts/activate/{code}/'
     send_mail(
         'Здравствуйте активируйте ваш аккаунт!',
         f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: {full_link}',
