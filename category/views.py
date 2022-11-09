9d7a735d68e796f97ed7dcd982beb67e4d56651c
turtas8 <turatbek.imankulov@gmail.com>
2022-11-09 15:45:24 +0600

    r4r43

--- category/views.py
+++ category/views.py
@@ -6,7 +6,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class StandartResultPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 3
     page_query_param = 'page'
     max_page_size = 1000
 
